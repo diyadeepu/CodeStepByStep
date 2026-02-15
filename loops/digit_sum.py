14b271297bdbb625e93b011d7f1dc86e0170b362
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-15 12:28:28 -0800

    Added digit_sum problem

---
+++ loops/digit_sum.py
@@ -0,0 +1,13 @@
+# Write a function named digit_sum that accepts an integer as a parameter and 
+# returns the sum of the digits of that number. For example, the call of 
+# digit_sum(29107) returns 2+9+1+0+7 or 19. For negative numbers, return the 
+# same value that would result if the number were positive. For example, 
+# digit_sum(-456) returns 4+5+6 or 15.
+
+def digit_sum(number: int) -> int:
+    number = abs(number)
+    sum = 0
+    while (number != 0):
+        sum += number % 10
+        number = number//10
+    return sum
