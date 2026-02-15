d96503fffd403af96fe48f4f951c6c8615b3b471
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-14 17:01:21 -0800

    Added longest_name problem

---
+++ if_else/longest_name.py
@@ -0,0 +1,19 @@
+# Write a function named longest_name that reads names typed by the user and 
+# prints the longest name (the name that contains the most characters) in the 
+# format shown below. Use the format shown below, which might result from a 
+# call of longest_name(4):
+
+def longest_name(n):
+    num = -100
+    for i in range(1, n+1):
+        name = input("name #" + str(i) + "? ")
+        if (len(name) == num):
+            vari = True
+        else:
+            vari = False
+        if (len(name) > num):
+            num = len(name)
+            finalN = name
+    print(finalN[0:1].upper() + finalN[1:].lower() + "'s name is longest")
+    if (vari == True):
+        print("(There was a tie!)")
