efa546423bf4376d734fe3fa8c6724b8b36308c6
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-16 16:26:53 -0800

    Added average_of_2 problem

---
+++ parameters_and_return/average_of_2.py
@@ -0,0 +1,6 @@
+# Write a function named average_of_2 that takes two integers as parameters and
+# returns the average of the two integers. For example, the call of 
+# average_of_2(2, 9) should return 5.5.
+
+def average_of_2(num1: int, num2: int) -> float:
+    return (num1+num2)/2
