b973206432dd2ead807ae8ca53ecd2fb17efe658
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-16 16:55:48 -0800

    Added days_in_months problem

---
+++ parameters_and_return/days_in_months.py
@@ -0,0 +1,13 @@
+# Write a function named days_in_month that accepts a month (an integer between
+# 1 and 12) as a parameter and returns the number of days in that month. For 
+# example, the call of days_in_month(9) returns 30 because September has 30 
+# days. You may assume that the month value passed is between 1 and 12 
+# inclusive. Ignore leap years assume that February always has 28 days.
+
+def days_in_month(m: int) -> int:
+    if (m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12):
+        return 31
+    elif (m == 4 or m == 6 or m == 9 or m == 11):
+        return 30
+    else:
+        return 28
