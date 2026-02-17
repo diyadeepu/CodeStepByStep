04c8f5eadcc169b5b868c96c7a40dc83d448e7fd
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-16 16:51:13 -0800

    Added count_digits problem

---
+++ parameters_and_return/count_digits.py
@@ -0,0 +1,12 @@
+# Write a function named count_digits that accepts an integer parameter and 
+# returns the number of digits in that integer. For example, count_digits(38015)
+# returns 5. For negative numbers, return the same value as if the number were 
+# positive. For example, count_digits(-72) returns 2.
+
+def count_digits(n: int) -> int:
+    n = abs(n)
+    count = 1
+    if (n > 0):
+        n /=10
+        count += 1
+    return count
