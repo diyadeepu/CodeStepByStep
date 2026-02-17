9619991f27e5357b28e5115dd500db4c16534d48
Diya Deepu <maildiyadeepu@gmail.com>
2026-02-16 16:28:39 -0800

    Added average_of_3 problem

---
+++ parameters_and_return/average_of_3.py
@@ -0,0 +1,6 @@
+# Write a function named average_of_3 that accepts three integers as parameters
+# and returns the average of the three integers as a number. For example, the 
+# call of average_of_3(4, 7, 13) returns 8.
+
+def average_of_3(num1: int, num2: int, num3: int) -> float:
+    return (num1+num2+num3)/3
